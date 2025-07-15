11cb6fa47b0e6c3c0c65ec619976a0a1bb08e776
CodingEdg <mohd2mehtab@gmail.com>
2025-07-15 09:36:07 +0530

    all file pushed

---
+++ backend/index.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import { connectDB } from './db';
+import availabilityRoutes from './routes/availabilityRoutes';
+import bookingRoutes from './routes/bookingRoutes';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+
+app.use(cors({
+  origin: 'http://localhost:3000', // adjust if your frontend runs elsewhere
+  credentials: true,              // if you're using cookies/auth
+}));
+
+
+app.use('/api/availability', availabilityRoutes);
+app.use('/api/bookingRoutes', bookingRoutes);
+
+
+connectDB().then(() => {
+});
+app.listen(PORT, () => {
+  console.log("============================================================================")
+  console.log("============================================================================")
+  console.log("============================================================================")
+  console.log("============================================================================")
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
